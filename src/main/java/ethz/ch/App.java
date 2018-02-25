a09beff872567a7e1b0af0403722cdb068e83715
Simon Huber <simipro@simipromac.local>
2018-02-25 15:09:26 +0100

    init

---
+++ src/main/java/ethz/ch/App.java
@@ -0,0 +1,13 @@
+package ethz.ch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
